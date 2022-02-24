479e7c191597f50721dce1004a5cd77f30ac2bd7
cemturker <cem.turker@messagebird.com>
2022-02-24 23:10:05 +0100

    Initial commit

---
+++ clouds/cmd/login.go
@@ -0,0 +1,24 @@
+package cmd
+
+import (
+	"fmt"
+	"rijn/clouds/gcd/login"
+
+	"github.com/spf13/cobra"
+)
+
+var loginCmd = &cobra.Command{
+	Use:   "login-gcd",
+	Short: "Logins into gcloud",
+	Long:  `Runs the following command "gcloud auth application-default login" login into the gcloud`,
+	Run: func(cmd *cobra.Command, args []string) {
+		login := login.NewLoginCmd()
+		if err := login.Execute(cmd.Context()); err != nil {
+			panic(fmt.Sprintf("unable to login gcloud err:%s", err))
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(loginCmd)
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "rijn/clouds/cmd"
+
+func main() {
+	cmd.Execute()
+}
