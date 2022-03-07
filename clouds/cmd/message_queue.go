f2524809b503d3139325cd3c09e341886385005c
cemturker <cem.turker@messagebird.com>
2022-03-07 23:53:54 +0100

    fix project shorthand

--- clouds/cmd/message_queue.go
+++ clouds/cmd/message_queue.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"rijn/clouds/gcd/pubsub"
+	"rijn/log"
 
 	"github.com/spf13/cobra"
 )
@@ -18,25 +19,25 @@ var pubsubCmd = &cobra.Command{
 	PreRun: func(cmd *cobra.Command, args []string) {
 		moveCmd, err := pubsub.NewMoveCmd(cmd.Context(), config)
 		if err != nil {
-			panic(err)
+			log.Fatal(err.Error())
 		}
 
 		moveCmdExecuter = moveCmd
 	},
 	Run: func(cmd *cobra.Command, args []string) {
 		if err := moveCmdExecuter.Execute(cmd.Context()); err != nil {
-			panic(err)
+			log.Fatal(err.Error())
 		}
 	},
 	PostRun: func(cmd *cobra.Command, args []string) {
 		if err := moveCmdExecuter.StopExecution(); err != nil {
-			panic(err)
+			log.Fatal(err.Error())
 		}
 	},
 }
 
 func init() {
-	pubsubCmd.PersistentFlags().StringVarP(&config.ProjectID, "project-id", "pId", "", "project Id")
+	pubsubCmd.PersistentFlags().StringVarP(&config.ProjectID, "project-id", "p", "", "project Id")
 	pubsubCmd.PersistentFlags().StringVarP(&config.Topic, "topic", "t", "", "topic name")
 	pubsubCmd.PersistentFlags().StringVarP(&config.Subscription, "subscription", "s", "", "subscription name")
 	rootCmd.AddCommand(pubsubCmd)
